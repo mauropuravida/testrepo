59f8ff13454cdb6b8eb32f887bf4bcb013ee985a
mauro <mauropuravida@gmail.com>
2021-05-25 23:50:24 -0300

    test commit

---
+++ example/index.js
@@ -0,0 +1,43 @@
+var volumemeter = require('../')
+var getusermedia = require('getusermedia')
+
+var left = document.querySelector('.left')
+var right = document.querySelector('.right')
+
+getusermedia({ audio: true, video: false }, function (err, stream) {
+  if (err) return console.error(err)
+
+  var ctx = new AudioContext
+  
+  var leftmeter = volumemeter(ctx, function (volume) {
+    left.style.height = volume + '%'
+  })
+
+  var rightmeter = volumemeter(ctx, function (volume) {
+    right.style.height = volume + '%'
+    console.log(document.getElementById("leftv").style.height)
+    if (volume > 30) {
+        alert("Estas hablando muy alto!")
+    }
+  })
+
+  var split = ctx.createChannelSplitter()
+  split.connect(leftmeter, 0, 0)
+  split.connect(rightmeter, 1, 0)
+  
+  var src = ctx.createMediaStreamSource(stream)
+  src.connect(split)
+  src.connect(ctx.createGain())
+
+  stream.onended = function () {
+    leftmeter.stop()
+    rightmeter.stop()
+  }
+})
+
+
+
+if (left.style.height > 30) {
+    console.log("gritando")
+}
+
